9b1aed7ab40ac143201b72c4186bf2da327737e5
S1ebaP <sebastianpieczara2016@gmail.com>
2023-03-13 15:59:09 +0000

    Added listbox

--- StoreApp/StoreApp/SummaryForm.Designer.cs
+++ StoreApp/StoreApp/SummaryForm.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.ExitSummaryButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // ExitSummaryButton
             // 
-            this.ExitSummaryButton.Location = new System.Drawing.Point(675, 389);
+            this.ExitSummaryButton.Location = new System.Drawing.Point(1012, 598);
+            this.ExitSummaryButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ExitSummaryButton.Name = "ExitSummaryButton";
-            this.ExitSummaryButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitSummaryButton.Size = new System.Drawing.Size(112, 35);
             this.ExitSummaryButton.TabIndex = 0;
             this.ExitSummaryButton.Text = "Exit";
             this.ExitSummaryButton.UseVisualStyleBackColor = true;
             this.ExitSummaryButton.Click += new System.EventHandler(this.ExitSummaryButton_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(259, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(327, 224);
+            this.listBox1.TabIndex = 1;
+            // 
             // SummaryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.ExitSummaryButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "SummaryForm";
             this.Text = "SummaryForm";
             this.ResumeLayout(false);
@@ -56,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button ExitSummaryButton;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
